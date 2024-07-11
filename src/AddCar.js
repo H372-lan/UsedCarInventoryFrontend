5f6bba9155f3389a1a3354289b50466c7311814a
Harshita Bajpai <78996496+H372-lan@users.noreply.github.com>
2024-07-11 12:18:46 +0530

    AddCar.js

--- src/AddCar.js
+++ src/AddCar.js
@@ -72,7 +72,7 @@ export default function AddCar() {
 
         delete errors.color;
       } else if (name === "milage") {
-        if (!/^[1-9_]+$/.test(value)) {
+        if (!/^[0-9_]+$/.test(value)) {
           error = "Milage Can not Contain special Character";
         }
 
